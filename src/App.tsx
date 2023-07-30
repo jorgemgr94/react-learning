ac3a3a54900df0a24499f0a5dd1b11492fa1cd4d
Jorge García <jorgemgr94@gmail.com>
2023-07-30 16:42:56 -0600

    feat: Upload file added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 import LoadingOrError from './components/LoadingOrError';
 const Debounce = lazy(async () => import('./pages/Debounce'));
+const UploadFile = lazy(async () => import('./pages/UploadFile'));
 
 export default function App() {
 	return (
@@ -10,6 +11,7 @@ export default function App() {
 			<Suspense fallback={<LoadingOrError />}>
 				<Routes>
 					<Route path="/debounce" element={<Debounce />} />
+					<Route path="/upload-file" element={<UploadFile />} />
 
 					{/* Navigate to /debounce as default route */}
 					<Route
