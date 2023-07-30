d35659f789486d51cc7d8cf99201fcb22e73f0d9
Jorge García <jorgemgr94@gmail.com>
2023-07-30 16:08:28 -0600

    fix: remove unused tests

--- src/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import App from '../src/App';
-
-test('App component display header', () => {
-	render(<App />);
-	const heading = screen.getByText(/Vite [+] React/i);
-	expect(heading).toBeInTheDocument();
-});
